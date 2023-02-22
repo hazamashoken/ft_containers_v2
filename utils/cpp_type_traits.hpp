c6cb9d26d6287e7b789c2999b840ff6c9ba0b471
tliangso <earth78203@gmail.com>
2023-02-23 01:02:53 +0800

    final 3

--- utils/cpp_type_traits.hpp
+++ utils/cpp_type_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 22:11:12 by tliangso          #+#    #+#             */
-/*   Updated: 2023/02/22 22:18:07 by tliangso         ###   ########.fr       */
+/*   Updated: 2023/02/23 01:02:36 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ namespace ft
 	struct __truth_type<true>
 	{ typedef __true_type __type; };
 
-	template <class _Sp, class _Tp>
-	struct __traitor
-	{
-		enum { __value = bool(_Sp::__value) || bool(_Tp::__value) };
-		typedef typename __truth_type<__value>::__type __type;
-	};
+	// template <class _Sp, class _Tp>
+	// struct __traitor
+	// {
+	// 	enum { __value = bool(_Sp::__value) || bool(_Tp::__value) };
+	// 	typedef typename __truth_type<__value>::__type __type;
+	// };
 
 	template <typename, typename>
 	struct __are_same
@@ -47,19 +47,19 @@ namespace ft
 		typedef __true_type __type;
 	};
 
-	template <typename _Tp>
-	struct __is_void
-	{
-		enum { __value = 0 };
-		typedef __false_type __type;
-	};
+	// template <typename _Tp>
+	// struct __is_void
+	// {
+	// 	enum { __value = 0 };
+	// 	typedef __false_type __type;
+	// };
 
-	template <>
-	struct __is_void<void>
-	{
-		enum { __value = 1 };
-		typedef __true_type __type;
-	};
+	// template <>
+	// struct __is_void<void>
+	// {
+	// 	enum { __value = 1 };
+	// 	typedef __true_type __type;
+	// };
 
 	template <typename _Tp>
 	struct __is_integer
@@ -159,99 +159,99 @@ namespace ft
 		typedef __true_type __type;
 	};
 
-	template <typename _Tp>
-	struct __is_floating
-	{
-		enum { __value = 0 };
-		typedef __false_type __type;
-	};
+	// template <typename _Tp>
+	// struct __is_floating
+	// {
+	// 	enum { __value = 0 };
+	// 	typedef __false_type __type;
+	// };
 
-	template <>
-	struct __is_floating<float>
-	{
-		enum { __value = 1 };
-		typedef __true_type __type;
-	};
+	// template <>
+	// struct __is_floating<float>
+	// {
+	// 	enum { __value = 1 };
+	// 	typedef __true_type __type;
+	// };
 
-	template <>
-	struct __is_floating<double>
-	{
-		enum { __value = 1 };
-		typedef __true_type __type;
-	};
+	// template <>
+	// struct __is_floating<double>
+	// {
+	// 	enum { __value = 1 };
+	// 	typedef __true_type __type;
+	// };
 
-	template <>
-	struct __is_floating<long double>
-	{
-		enum { __value = 1 };
-		typedef __true_type __type;
-	};
+	// template <>
+	// struct __is_floating<long double>
+	// {
+	// 	enum { __value = 1 };
+	// 	typedef __true_type __type;
+	// };
 
-	template <typename _Tp>
-	struct __is_pointer
-	{
-		enum { __value = 0 };
-		typedef __false_type __type;
-	};
+	// template <typename _Tp>
+	// struct __is_pointer
+	// {
+	// 	enum { __value = 0 };
+	// 	typedef __false_type __type;
+	// };
 
-	template <typename _Tp>
-	struct __is_pointer<_Tp*>
-	{
-		enum { __value = 1 };
-		typedef __true_type __type;
-	};
+	// template <typename _Tp>
+	// struct __is_pointer<_Tp*>
+	// {
+	// 	enum { __value = 1 };
+	// 	typedef __true_type __type;
+	// };
 
-	template <typename _Tp>
-	struct __is_arithmetic
-	: public __traitor<__is_integer<_Tp>, __is_floating<_Tp> >
-	{ };
+	// template <typename _Tp>
+	// struct __is_arithmetic
+	// : public __traitor<__is_integer<_Tp>, __is_floating<_Tp> >
+	// { };
 
-	template <typename _Tp>
-	struct __is_scalar
-	: public __traitor<__is_arithmetic<_Tp>, __is_pointer<_Tp> >
-	{ };
+	// template <typename _Tp>
+	// struct __is_scalar
+	// : public __traitor<__is_arithmetic<_Tp>, __is_pointer<_Tp> >
+	// { };
 
-	template <typename _Tp>
-	struct __is_char
-	{
-		enum { __value = 0 };
-		typedef __false_type __type;
-	};
+	// template <typename _Tp>
+	// struct __is_char
+	// {
+	// 	enum { __value = 0 };
+	// 	typedef __false_type __type;
+	// };
 
-	template <>
-	struct __is_char<char>
-	{
-		enum { __value = 1 };
-		typedef __true_type __type;
-	};
+	// template <>
+	// struct __is_char<char>
+	// {
+	// 	enum { __value = 1 };
+	// 	typedef __true_type __type;
+	// };
 
-	template <typename _Tp>
-	struct __is_byte
-	{
-		enum { __value = 0 };
-		typedef __false_type __type;
-	};
+	// template <typename _Tp>
+	// struct __is_byte
+	// {
+	// 	enum { __value = 0 };
+	// 	typedef __false_type __type;
+	// };
 
-	template <>
-	struct __is_byte<char>
-	{
-		enum { __value = 1 };
-		typedef __true_type __type;
-	};
+	// template <>
+	// struct __is_byte<char>
+	// {
+	// 	enum { __value = 1 };
+	// 	typedef __true_type __type;
+	// };
 
-	template <>
-	struct __is_byte<signed char>
-	{
-		enum { __value = 1 };
-		typedef __true_type __type;
-	};
+	// template <>
+	// struct __is_byte<signed char>
+	// {
+	// 	enum { __value = 1 };
+	// 	typedef __true_type __type;
+	// };
 
-	template <>
-	struct __is_byte<unsigned char>
-	{
-		enum { __value = 1 };
-		typedef __true_type __type;
-	};
+	// template <>
+	// struct __is_byte<unsigned char>
+	// {
+	// 	enum { __value = 1 };
+	// 	typedef __true_type __type;
+	// };
 
 }
 
